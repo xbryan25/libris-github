002c47cb9dbd08bece1a31501d369c3ce5ca442b
Ordinary33 <husnie.baddie@g.msuiit.edu.ph>
2025-11-23 18:30:50 +0800

    fix: added additional condition for disabling the send request button

--- app/nuxt/app/components/RentBookModal.vue
+++ app/nuxt/app/components/RentBookModal.vue
@@ -347,10 +347,10 @@ async function sendRental() {
           </UButton>
           <UButton
             @click.stop.prevent="sendRental"
-            :disabled="!!timeError || !meetupStartTime || !meetupEndTime || !finalDays || !meetupDate || !meetupAddressQuery || loading || hasInsufficientFunds"
+            :disabled="!!timeError || !meetupStartTime || !meetupEndTime || !finalDays || !meetupDate || !meetupAddressQuery || loading || hasInsufficientFunds || isSendingRental"
             class="bg-slate-800 hover:bg-slate-700 text-white dark:bg-slate-700 dark:hover:bg-slate-600 px-4 py-2 rounded disabled:bg-slate-600 disabled:dark:bg-slate-500 disabled:cursor-not-allowed"
           >
-            <p v-if="!loading">Send Rental Request</p> 
+            <p v-if="!loading && !isSendingRental">Send Rental Request</p> 
             <p v-else>Sending...</p>
           </UButton>
         </div>
